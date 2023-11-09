d673caedb97b714abfcd91da3245fd0dfdaec6ca
ms606 <muffi606@gmail.com>
2023-11-09 12:26:40 +0500

    loader for mobi

--- src/components/Layout/Designer.tsx
+++ src/components/Layout/Designer.tsx
@@ -696,6 +696,7 @@ const Designer: FC<{ onCloseClick?: () => void }> = ({ onCloseClick }) => {
 			)}
 			{moveElements && (
 				<ZakekeDesignerContainer isMobile={isMobile} className='zakeke-container'>
+					<div style={{width: "100%", height: "95%"}}>
 					<ZakekeDesigner ref={customizerRef} areaId={actualAreaId} />
 					<IconsAndDesignerContainer>
 						{/* <ZoomIconIn hoverable onClick={() => customizerRef.current.zoomIn()}>
@@ -708,6 +709,7 @@ const Designer: FC<{ onCloseClick?: () => void }> = ({ onCloseClick }) => {
 					<Button isFullWidth onClick={() => setMoveElements(false)}>
 						<span>OK</span>
 					</Button>
+					</div>
 				</ZakekeDesignerContainer>
 			)}
 		</>
