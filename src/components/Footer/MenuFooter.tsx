1c000d30d4c9cfae7dfa3172ace07e75f403f94a
ms606 <muffi606@gmail.com>
2023-12-04 18:10:38 +0500

    refactor menu footer

--- src/components/Footer/MenuFooter.tsx
+++ src/components/Footer/MenuFooter.tsx
@@ -1,41 +1,46 @@
-
 import React from 'react'
 import ShareIcon from "../../icons/ShareIcon";
+import { useZakeke, Group } from "zakeke-configurator-react";
+import useStore from "../../Store";
 
-// type MenuFooterProps {
-//     viewFooter: any;
 
-// }
 
-// const MenuFooter:<MenuFooterProps> = () => {
-//     return (<div>
-//           <div className="menu_footer" ref={viewFooter}>
-//           <div className="menu_price">
-//             <div className="price_text">Price: </div>
-//             <div className="price_value">{priceFormatter.format(price)}</div>
-//           </div>
-//           <div className="menu_actions">
-//             {isAddToCartLoading ? (
-//               "Adding to cart..."
-//             ) : (
-//               <button
-//                 onClick={addToCart}
-//                 className="btn btn-primary menu_btn_cart"
-//               >
-//                 Add to cart
-//               </button>
-//             )}
-//             {
-//               <button className="btn btn-secondary Menu_ff_menu__btn__iOQsk Menu_ff_menu__btn__share__1sacu">
-//                 <div className="menu_btn_share_icon">
-//                   <ShareIcon />
-//                 </div>
-//                 {/* <span className="Menu_ff_menu__btn__share_text__3eeX0">Share</span> */}
-//               </button>
-//             }
-//           </div>
-//         </div>
-//     </div>)
-// }
+interface MenuFooterProps {
+    viewFooter: any;
+}
 
-// export default MenuFooter
+const MenuFooter:React.FC<MenuFooterProps> = ({viewFooter}) => {
+
+    const { isAddToCartLoading, addToCart, price} = useZakeke();
+    const { priceFormatter} = useStore();
+
+    return (<div>
+          <div className="menu_footer" ref={viewFooter}>
+          <div className="menu_price">
+            <div className="price_text">Price: </div>
+            <div className="price_value">{priceFormatter.format(price)}</div>
+          </div>
+          <div className="menu_actions">
+            {isAddToCartLoading ? (
+              "Adding to cart..."
+            ) : (
+              <button
+                onClick={addToCart}
+                className="btn btn-primary menu_btn_cart"
+              >
+                Add to cart
+              </button>
+            )}
+            {
+              <button className="btn btn-secondary Menu_ff_menu__btn__iOQsk Menu_ff_menu__btn__share__1sacu">
+                <div className="menu_btn_share_icon">
+                  <ShareIcon />
+                </div>  
+              </button>
+            }
+          </div>
+        </div>
+    </div>)
+}
+
+export default MenuFooter
