d08c5f2d8275af73d5c6c2d08e31f45d7d1b3def
ms606 <muffi606@gmail.com>
2024-02-10 23:01:11 +0500

    drop down list name

--- src/components/widgets/ItemText.tsx
+++ src/components/widgets/ItemText.tsx
@@ -1,126 +1,110 @@
-import React from 'react';
+import React from "react";
 import { FC, useCallback } from "react";
 import styled from "styled-components/macro";
-import { FontFamily, useZakeke } from 'zakeke-configurator-react';
-import { Button, Columns, Icon, TextArea } from '../Atomic';
-import Select, { components, GroupBase, OptionProps, SingleValueProps } from 'react-select';
+import { FontFamily, useZakeke } from "zakeke-configurator-react";
+import { Button, Columns, Icon, TextArea } from "../Atomic";
+// import Select, { components, GroupBase, OptionProps, SingleValueProps } from 'react-select';
+import {
+  CSSObjectWithLabel,
+  GroupBase,
+  OptionProps,
+  SingleValueProps,
+  components,
+} from "react-select";
 import { useState } from "react";
-import { debounce } from 'lodash';
+import { debounce } from "lodash";
 
 import type { PropChangeHandler } from "../Layout/Designer";
 
-import { ReactComponent as CloseIcon } from '../../assets/icons/times-solid.svg'
-import { ReactComponent as BoldIcon } from '../../assets/icons/times-solid.svg'
-import { ReactComponent as ItalicSolid } from '../../assets/icons/times-solid.svg'
-import { ReactComponent as CurveIcon } from '../../assets/icons/times-solid.svg'
+import { ReactComponent as CloseIcon } from "../../assets/icons/times-solid.svg";
+import { ReactComponent as BoldIcon } from "../../assets/icons/times-solid.svg";
+import { ReactComponent as ItalicSolid } from "../../assets/icons/times-solid.svg";
+import { ReactComponent as CurveIcon } from "../../assets/icons/times-solid.svg";
 import { FormControl } from "./FormControl";
 //import ColorPicker from "./colorpicker";
+import AdvancedSelect from './AdvancedSelect';
 
 export interface EditTextItem {
-    guid: string,
-    name: string,
-    text: string,
-    fillColor: string,
-    fontFamily: string,
-    fontWeight: string,
-    fontSize: number,
-    isTextOnPath: boolean;
-    constraints: { [key: string]: any } | null,
+  guid: string;
+  name: string;
+  text: string;
+  fillColor: string;
+  fontFamily: string;
+  fontWeight: string;
+  fontSize: number;
+  isTextOnPath: boolean;
+  constraints: { [key: string]: any } | null;
 }
 
-const defaultColorsPalette = ['#000000', '#FFFFFF'];
+const defaultColorsPalette = ["#000000", "#FFFFFF"];
 
 enum ItemType {
-    Text = 0,
-    Image = 1
+  Text = 0,
+  Image = 1,
 }
 
 export interface TextItem {
-    type: ItemType;
-    areaId: number;
-    guid: string;
-    name: string;
-    text: string;
-    strokeColor: string;
-    strokeWidth: number;
-    fillColor: string;
-    fontFamily: string;
-    fontSize: number;
-    fontWeight: string;
-    fontStyle: string | undefined;
-    fontStretch: string;
-    justification: string;
-    isTextOnPath: boolean;
-    constraints: ({
-        [key: string]: any;
-    }) | null;
+  type: ItemType;
+  areaId: number;
+  guid: string;
+  name: string;
+  text: string;
+  strokeColor: string;
+  strokeWidth: number;
+  fillColor: string;
+  fontFamily: string;
+  fontSize: number;
+  fontWeight: string;
+  fontStyle: string | undefined;
+  fontStretch: string;
+  justification: string;
+  isTextOnPath: boolean;
+  constraints: {
+    [key: string]: any;
+  } | null;
 }
 
-const ItemTextContainer = styled.div`
-`;
+const ItemTextContainer = styled.div``;
 
 const TextToolsContainer = styled.div`
-  display:flex;
-  flex-direction:row;
-  grid-gap:10px;  
-  flex-wrap:wrap;
+  display: flex;
+  flex-direction: row;
+  grid-gap: 10px;
+  flex-wrap: wrap;
   top: 20%;
 `;
 
 const TextButtonsContainer = styled.div`
-    width:50%;
-    display: grid;
-    grid-template-columns:1fr 1fr;
-    grid-gap:5px;
+  width: 50%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 5px;
 `;
 
 const ColorPickerContainer = styled.div`
-  margin-right:5px;
+  margin-right: 5px;
   width: calc(50% - 30px);
 `;
 
 const ColorsContainer = styled.div`
-  display:flex;
-  flex-direction:row;
+  display: flex;
+  flex-direction: row;
   padding-bottom: 20px;
-  border-bottom: 1px #ccc dotted;  
-`;
-
-const SinglePaletteItem = styled.div<{ color: string, selected: boolean }>`
-    width: 20px;
-    height: 20px;
-    background-color: ${props => props.color};
-    border: 1px lightgray solid;
-    cursor:pointer;
-    
-    ${props => props.selected && `border: 1px black solid;`}
-
-    &:hover {
-        opacity: 0.6;
-    }
-`;
-
-const TextColorsContainer = styled.div<{ isDefaultPalette?: boolean }>`
-    display: grid;
-    ${props => !props.isDefaultPalette && `
-    grid-template-columns: repeat(auto-fill,minmax(20px,1fr));
-    grid-gap: 5px;`};
-    /* grid-template-columns: repeat(auto-fill,minmax(20px,1fr)); */
-    width: 100%;
+  border-bottom: 1px #ccc dotted;
 `;
 
 const OptionContainer = styled(components.Option)`
-    background-color: white !important;
-    
-    &:hover {
-        background-color: #DDD !important;
-    }
+  background-color: white !important;
 
-    img {
-        max-width: 100%;
-        height: 4px;
-        object-fit: contain;
-    }
+  &:hover {
+    background-color: #ddd !important;
+  }
+
+  img {
+    max-width: 100%;
+    height: 4px;
+    object-fit: contain;
+  }
 `;
 
 const SingleValueContainer = styled(components.SingleValue)`
@@ -131,74 +115,198 @@ const SingleValueContainer = styled(components.SingleValue)`
     }
 `;
 
+
+const SinglePaletteItem = styled.div<{ color: string; selected: boolean }>`
+	width: 20px;
+	height: 20px;
+	background-color: ${(props) => props.color};
+	border: 1px lightgray solid;
+	cursor: pointer;
+
+	${(props) => props.selected && `border: 1px black solid;`}
+
+	&:hover {
+		opacity: 0.6;
+	}
+`;
+
+const TextColorsContainer = styled.div<{ $isDefaultPalette?: boolean }>`
+	display: grid;
+	${(props) =>
+		!props.$isDefaultPalette &&
+		`
+    grid-template-columns: repeat(auto-fill,minmax(20px,1fr));
+    grid-gap: 5px;`};
+	/* grid-template-columns: repeat(auto-fill,minmax(20px,1fr)); */
+	width: 100%;
+`;
+
+const FontCustomOption = styled.img`
+	max-width: 100%;
+	height: 24px;
+	object-fit: contain;
+`;
+
+const FontCustomSingleValue = styled.img`
+	max-width: 100%;
+	height: 24px;
+	object-fit: contain;
+`;
+
+const FontCustomSingleValueContainer = styled.div`
+	display: flex;
+	place-content: center;
+	width: 100%;
+	height: 100%;
+`;
+
 const FontOption = (props: JSX.IntrinsicAttributes & OptionProps<any, boolean, GroupBase<any>>) => {
-    return (
-        <OptionContainer {...props}>
-            {<img src={props.data.imageUrl} alt={props.data.name} />}
-        </OptionContainer>
-    );
-}
+	return (
+		<components.Option {...props}>
+			{<FontCustomOption src={props.data.imageUrl} alt={props.data.name} />}
+		</components.Option>
+	);
+};
 
 const FontSingleValue = (props: JSX.IntrinsicAttributes & SingleValueProps<any, boolean, GroupBase<any>>) => {
+	return (
+		<components.SingleValue {...props}>
+			<FontCustomSingleValueContainer>
+				{<FontCustomSingleValue src={props.data.imageUrl} alt={props.data.name} />}
+			</FontCustomSingleValueContainer>
+		</components.SingleValue>
+	);
+};
+const ItemText: FC<{
+  item: EditTextItem;
+  handleItemPropChange: PropChangeHandler;
+  fonts?: FontFamily[];
+  hideRemoveButton?: boolean;
+}> = ({ item, handleItemPropChange, hideRemoveButton }) => {
+  const { removeItem, fonts, disableTextColors, textColors } = useZakeke();
+
+  console.log(fonts, "fonts");
+
+  const constraints = item.constraints;
+  const canEdit = constraints?.canEdit ?? true;
+  const hasCurvedText = item.isTextOnPath;
+  const isUpperCase = constraints?.toUppercase ?? false;
+
+  // Used for performance cache
+  const [fillColor, setFillColor] = useState(item.fillColor);
+
+  const weightData =
+    typeof item.fontWeight === "number"
+      ? ["normal", "normal"]
+      : item.fontWeight.split(" ");
+  const isBold =
+    weightData.length > 1 ? weightData[1] === "bold" : weightData[0] === "bold";
+  const isItalic = weightData.length > 1 ? weightData[0] === "italic" : false;
+  const [textAreaLength, setTextAreaLength] = useState(item.text.length);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let stringWithZeroWidthSpace = e.target.value.replace(/\u200B/g, "");
+
+    console.log(stringWithZeroWidthSpace.length);
+
+    setTextAreaLength(e.target.value.length);
+    if (handleItemPropChange)
+      handleItemPropChange(
+        item as TextItem,
+        "text",
+        isUpperCase
+          ? e.currentTarget.value.toUpperCase()
+          : e.currentTarget.value
+      );
+  };
+
+  //eslint-disable-next-line
+  const handleFillColorChange = useCallback(
+    debounce((color: string) => {
+      handleItemPropChange(item, "font-color", color);
+    }, 500),
+    []
+  );
+
+  let currentFont = fonts?.find((x) => x.name === item.fontFamily);
+
+  const [fontLoading, setFontLoading] = useState(false);
+
+  const setItemTextDebounced = (value: string) => {
+    handleItemPropChange?.(item, 'text', isUpperCase ? value.toUpperCase() : value);
+    debounce(() => {
+        const initialText = value;
+        
+        const text = value;
+
+        if (text !== initialText) {
+            handleItemPropChange?.(item, 'text', isUpperCase ? text.toUpperCase() : text);
+        }
+    }, 500)();
+};
+
+  const handleFontChange = (font: string) => {
+    handleItemPropChange(item, 'font-family', font);
+    currentFont = fonts?.find((x) => x.name === font);
+    setItemTextDebounced(item.text);
+};
+
+
+
+  if (item)
     return (
-        <SingleValueContainer {...props}>
-            {<img src={props.data.imageUrl} alt={props.data.name} />}
-        </SingleValueContainer>
-    );
-}
+      <ItemTextContainer>
+        <FormControl
+          label={item.name || "Add Text"}
+          // rightComponent={!hideRemoveButton && item.constraints!.canDelete
+          // && <Icon onClick={() => removeItem(item.guid)}><CloseIcon /></Icon>}
+        >
+          <TextArea
+            value={isUpperCase ? item.text.toUpperCase() : item.text}
+            onChange={handleChange}
+            maxLength={
+              !item.constraints ? null : item.constraints.maxNrChars || null
+            }
+            disabled={!canEdit}
+          />
+          <span
+            style={{ position: "relative", paddingTop: "25px", right: "23px" }}
+          >
+            {textAreaLength}
+          </span>
+        </FormControl>
 
-const ItemText: FC<{ item: EditTextItem, handleItemPropChange: PropChangeHandler, fonts?: FontFamily[], hideRemoveButton?: boolean }> = ({ item, handleItemPropChange, hideRemoveButton }) => {
-    const { removeItem, fonts, disableTextColors, textColors } = useZakeke();
+        <TextToolsContainer>
+          {(!constraints || constraints.canChangeFontFamily) && (
+            <FormControl label={"Font"}>
+              <AdvancedSelect
+                components={{
+                  Option: FontOption,
+                  SingleValue: FontSingleValue,
+                }}
+                styles={{
+                  container: (base) =>
+                    ({
+                      ...base,
+                      width: 200,
+                    } as CSSObjectWithLabel),
+                }}
+                isSearchable={false}
+                options={fonts}
+                isDisabled={fontLoading}
+                menuPosition="fixed"
+                value={[fonts!.find((x) => x.name === item.fontFamily)]}
+                onChange={(font: any) => {
+                  item.fontFamily = font.name;
+                  setFontLoading(true);
+                  handleFontChange(font.name);
+                  setTimeout(() => {
+                    setFontLoading(false);
+                  }, 2000);
+                }}
+              />
 
-    const constraints = item.constraints;
-    const canEdit = constraints?.canEdit ?? true;
-    const hasCurvedText = item.isTextOnPath;
-    const isUpperCase = constraints?.toUppercase ?? false;
-
-    // Used for performance cache
-    const [fillColor, setFillColor] = useState(item.fillColor);
-
-    const weightData = typeof item.fontWeight === 'number' ? ['normal', 'normal'] : item.fontWeight.split(' ');
-    const isBold = weightData.length > 1 ? weightData[1] === 'bold' : weightData[0] === 'bold';
-    const isItalic = weightData.length > 1 ? weightData[0] === 'italic' : false;
-    const [textAreaLength, setTextAreaLength] = useState(item.text.length);
-
-    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        
-        let stringWithZeroWidthSpace = e.target.value.replace(/\u200B/g, '');
-
-        console.log(stringWithZeroWidthSpace.length);
-        
-        setTextAreaLength(e.target.value.length);
-        if (handleItemPropChange)
-            handleItemPropChange((item as TextItem), 'text', (isUpperCase ? (e.currentTarget.value).toUpperCase() : e.currentTarget.value));
-    }
-
-    //eslint-disable-next-line
-    const handleFillColorChange = useCallback(debounce((color: string) => {
-        handleItemPropChange(item, 'font-color', color);
-    }, 500), []);
-
-    if (item)
-        return <ItemTextContainer>
-            <FormControl 
-                label={item.name || "Add Text"}
-                // rightComponent={!hideRemoveButton && item.constraints!.canDelete 
-                // && <Icon onClick={() => removeItem(item.guid)}><CloseIcon /></Icon>}
-            >
-
-                <TextArea
-                    value={isUpperCase ? item.text.toUpperCase() : item.text}
-                    onChange={handleChange}
-                    maxLength={!item.constraints ? null : (item.constraints.maxNrChars || null)}
-                    disabled={!canEdit} />
-                <span style={{position: 'relative', paddingTop: '25px', right: '23px'}}>{textAreaLength}</span>
-            </FormControl>
-            
-
-            <TextToolsContainer>
-                {(!constraints || constraints.canChangeFontFamily) && <FormControl label={"Font"}>
-                    <Select
+              {/* <Select
                         styles={{
                             container: base => ({
                                 ...base,
@@ -216,9 +324,10 @@ const ItemText: FC<{ item: EditTextItem, handleItemPropChange: PropChangeHandler
                         }}
                         value={[fonts!.find(x => x.name === item.fontFamily)]}
                         onChange={(font: any) => handleItemPropChange(item, 'font-family', font.name)}
-                    />
-                </FormControl>}
-                {/* <TextButtonsContainer>
+                    /> */}
+            </FormControl>
+          )}
+          {/* <TextButtonsContainer>
                     {(!constraints || constraints.canChangeFontWeight) && <FormControl label={"Style"}>
                         <Columns columns={2}>
                             <Button
@@ -244,9 +353,9 @@ const ItemText: FC<{ item: EditTextItem, handleItemPropChange: PropChangeHandler
                         </Button>
                     </FormControl>}
                 </TextButtonsContainer> */}
-            </TextToolsContainer>
+        </TextToolsContainer>
 
-            {/* {(!disableTextColors || !(disableTextColors && textColors.length === 1)) && <FormControl label={T._("Color", "Composer")}>
+        {/* {(!disableTextColors || !(disableTextColors && textColors.length === 1)) && <FormControl label={T._("Color", "Composer")}>
                 <ColorsContainer>
                     {!disableTextColors && <ColorPickerContainer>
                         <ColorPicker
@@ -283,9 +392,9 @@ const ItemText: FC<{ item: EditTextItem, handleItemPropChange: PropChangeHandler
                     </TextColorsContainer>}
                 </ColorsContainer>
             </FormControl>} */}
-        </ItemTextContainer >
-    else
-        return null;
-}
+      </ItemTextContainer>
+    );
+  else return null;
+};
 
 export default ItemText;
