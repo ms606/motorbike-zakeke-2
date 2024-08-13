0361c79a6c36ad4c148c88c59a30ed540309e804
muffi606 <muffi606@gmail.com>
2024-08-13 15:31:19 +0500

    Finnish

--- src/components/Footer/MenuFooter.tsx
+++ src/components/Footer/MenuFooter.tsx
@@ -41,7 +41,7 @@ const MenuFooter: React.FC<MenuFooterProps> = ({ viewFooter }) => {
               }
               className="btn btn-primary menu_btn_cart"
             >
-              NEXT
+              FINNISH
             </div>
           )}
           {/* {
